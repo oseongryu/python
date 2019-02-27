b5132c31e4b89604c9d98c3ca2efe6e5e9f64881
f5074 <oseongryu@gmail.com>
2019-02-27 18:57:06 +0900

    Add file dialog

--- c_program/e_program/program_3.py
+++ c_program/e_program/program_3.py
@@ -11,28 +11,34 @@ import time
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import csv
 
 conn = sqlite3.connect("sqlitedb.db")
 
 cur = conn.cursor()
-# cur.execute('DROP TABLE IF EXISTS stocks')
+cur.execute('DROP TABLE IF EXISTS stocks')
 
-# cur.execute\
-# ("""
-# CREATE TABLE stocks
-# (   id text
-# , pw text
-# , site text
-# , log text
-# )
-# """)
-# conn.commit()
+cur.execute\
+("""
+CREATE TABLE stocks
+(   id text
+, pw text
+)
+""")
+conn.commit()
 
 
 form_class = uic.loadUiType("radiobuttion.ui")[0]
 
+# table 리스트
+def selectTableList():
+    cur = conn.cursor()
+    sql = "SELECT id, pw FROM stocks"
+    cur.execute(sql)
 
+    rows = cur.fetchall()
 
+    return rows
 
 class MyWindow(QMainWindow, form_class):
     def __init__(self):
@@ -55,6 +61,9 @@ class MyWindow(QMainWindow, form_class):
         # self.comboBox_3.activated[str].connect(self.combo_box_1_changed)
 
         self.pushButton.clicked.connect(self.click_button_start)
+        self.pushButton_3.clicked.connect(self.openFileNameDialog)
+        self.pushButton_4.clicked.connect(self.loadFile)
+
 
         # self.checkBox.setChecked(True)
         # self.checkBox_2.setChecked(True)
@@ -70,6 +79,9 @@ class MyWindow(QMainWindow, form_class):
         # 테이블 수정 불가능하게 변경
         self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)  # row 단위로 선택 가능
+
+        self.plainTextEdit_5.setDisabled(1)
+
     # 테이블 클릭
     def btn_table_clicked(self):
         try:
@@ -77,12 +89,11 @@ class MyWindow(QMainWindow, form_class):
 
             for idx in tw_si:
                 pass
-
             # row의 0번째
             item = self.tableWidget.item(idx.row(), 0)
             if item is not None:
                 txt = item.text()
-                self.lineEdit.setText(txt)
+                # self.lineEdit.setText(txt)
             else:
                 txt = "no data"
 
@@ -90,14 +101,15 @@ class MyWindow(QMainWindow, form_class):
             item = self.tableWidget.item(idx.row(), 1)
             if item is not None:
                 txt = item.text()
-                self.lineEdit_2.setText(txt)
+                # self.lineEdit_2.setText(txt)
             else:
                 txt = "no data"
-
 
         except Exception as e:
             print(e)
             print(type(e))
+            self.statusbar.showMessage("[Error] " + str(type(e)))
+            return
 
     def click_button_start(self):
         try:
@@ -110,12 +122,14 @@ class MyWindow(QMainWindow, form_class):
             plain_text_edit_id = self.plainTextEdit_2.toPlainText()
             plain_text_edit_pw = self.plainTextEdit_3.toPlainText()
             plain_text_edit_login_button = self.plainTextEdit_4.toPlainText()
+        except Exception as e:
+            print(e)
+            print(type(e))
+            self.statusbar.showMessage("[Error] " + str(type(e)))
+            return
 
-            # 디렉토리가 없을 경우 디렉토리 생성
-            dirname = './screenshot'
-            if not os.path.isdir(dirname):
-                os.mkdir(dirname)
 
+        try:
             #  사이트주소, 아이디, 패스워드, 로그인버튼 위치값 여부 확인
             if(plain_text_edit_url== ""):
                 self.statusbar.showMessage('사이트 주소가 비었음')
@@ -143,19 +157,48 @@ class MyWindow(QMainWindow, form_class):
                 # UserAgent값 변경
                 options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
 
-            driver = webdriver.Chrome('C:\\Temp\\chromedriver.exe', chrome_options=options)
+            driver = webdriver.Chrome('chromedriver.exe', chrome_options=options)
+        except Exception as e:
+            print(e)
+            print(type(e))
+            self.statusbar.showMessage("[Error] 크롬드라이버 경로를 잘못 입력하였습니다." + str(type(e)))
+            return
 
+        try:
             # 3초 대기하기
             driver.implicitly_wait(3)
 
             # url 읽어들이기
             driver.get(plain_text_edit_url)
 
+        except Exception as e:
+            print(e)
+            print(type(e))
+            self.statusbar.showMessage("[Error] 사이트주소경로를 잘못 입력하였습니다." + str(type(e)))
+            return
 
+        try:
+            # 디렉토리가 없을 경우 디렉토리 생성
+            dirname = './screenshot'
+            if not os.path.isdir(dirname):
+                os.mkdir(dirname)
+        except Exception as e:
+            print(e)
+            print(type(e))
+            self.statusbar.showMessage("[Error] 디렉토리 생성을 잘못하였습니다." + str(type(e)))
+            return
+
+        try:
             # 시작전 스크린샷이 True일 경우 수행
             if(self.checkBox_5.isChecked()):
                 driver.save_screenshot("./screenshot/"+"aaaa" +"_"+"bbbb"+"_1(before).png")
+        except Exception as e:
+            print(e)
+            print(type(e))
+            self.statusbar.showMessage("[Error] " + str(type(e)))
+            return
 
+        try:
             if(combo_box_id == "id"):
                 driver.find_element_by_id(plain_text_edit_id).clear()
                 driver.find_element_by_id(plain_text_edit_id).send_keys("ididid")
@@ -165,8 +208,13 @@ class MyWindow(QMainWindow, form_class):
             elif(combo_box_id == "xpath"):
                 driver.find_element_by_xpath(plain_text_edit_id).clear()
                 driver.find_element_by_xpath(plain_text_edit_id).send_keys("ididid")
+        except Exception as e:
+            print(e)
+            print(type(e))
+            self.statusbar.showMessage("[Error] 아이디경로를 잘못 입력하였습니다." + str(type(e)))
+            return
 
-
+        try:
             if(combo_box_pw == "id"):
                 driver.find_element_by_id(plain_text_edit_pw).clear()
                 driver.find_element_by_id(plain_text_edit_pw).send_keys("ididid")
@@ -177,18 +225,36 @@ class MyWindow(QMainWindow, form_class):
                 driver.find_element_by_xpath(plain_text_edit_pw).clear()
                 driver.find_element_by_xpath(plain_text_edit_pw).send_keys("ididid")
 
+        except Exception as e:
+            print(e)
+            print(type(e))
+            self.statusbar.showMessage("[Error] 패스워드경로를 잘못 입력하였습니다." + str(type(e)))
+            return
+
+        try:
             # 아이디비번입력후 스크린샷 찍을 경우 수행
             if(self.checkBox_6.isChecked()):
                 driver.save_screenshot("./screenshot/"+"aaaa" +"_"+"bbbb"+"_2(after).png")
+        except Exception as e:
+            print(e)
+            print(type(e))
+            self.statusbar.showMessage("[Error] " + str(type(e)))
+            return
 
-
+        try:
             if(combo_box_login_button == "id"):
                 driver.find_element_by_id(plain_text_edit_login_button).click()
             elif(combo_box_login_button == "name"):
                 driver.find_element_by_name(plain_text_edit_login_button).click()
             elif(combo_box_login_button == "xpath"):
                 driver.find_element_by_xpath(plain_text_edit_login_button).click()
+        except Exception as e:
+            print(e)
+            print(type(e))
+            self.statusbar.showMessage("[Error] 로그인버튼경로를 잘못 입력하였습니다." + str(type(e)))
+            return
 
+        try:
             #경고창 체크박스가 True일 경우 수행
             if(self.checkBox_2.isChecked()):
                 WebDriverWait(driver, 3).until(EC.alert_is_present(),'Timed out waiting for PA creation confirmation popup to appear.')
@@ -196,19 +262,69 @@ class MyWindow(QMainWindow, form_class):
                 print(alert.text)
                 alert.accept()
                 # print("alert accepted")
-
         except Exception as e:
             print(e)
             print(type(e))
-            self.statusbar.showMessage("[Error]" + str(type(e)))
+            self.statusbar.showMessage("[Error] 경고창이 존재하지 않습니다." + str(type(e)))
+            return
 
-    def combo_box_1_changed(self,text):
+    def openFileNameDialog(self):
         try:
-            msg = self.comboBox.currentText()
-            self.statusbar.showMessage(msg + "선택 됨")
+            options = QFileDialog.Options()
+            options |= QFileDialog.DontUseNativeDialog
+            # fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "","csv Files (*.csv);;All Files (*)", options=options)
+            fileName, _ = QFileDialog.getOpenFileName(self, "csv 파일을 선택하세요.", "","csv Files (*.csv);;All Files (*)", options=options)
+
+            if fileName:
+                print(fileName)
+                self.plainTextEdit_5.clear()
+                self.plainTextEdit_5.insertPlainText(fileName)
         except Exception as e:
             print(e)
             print(type(e))
+            self.statusbar.showMessage("[Error] 경고창이 존재하지 않습니다." + str(type(e)))
+            return
+
+    def loadFile(self):
+        try:
+            plain_text_edit_csv_file_dir = self.plainTextEdit_5.toPlainText().replace(r"\\",r"\\")
+            f = open(plain_text_edit_csv_file_dir, 'r')  # open the csv data file
+            # next(f, None) # skip the header row
+            reader = csv.reader(f)
+            for row in reader:
+                cur.execute('INSERT INTO  stocks VALUES (?,?)', row)
+            conn.commit()
+
+            for row in cur.execute("SELECT * FROM stocks"):
+                print(row)
+            f.close()
+
+            # conn.close()
+            self.pushButton_3.setDisabled(1)
+            self.pushButton_4.setDisabled(1)
+
+
+            # 조회
+            rows = selectTableList()
+            self.tableWidget.setRowCount(len(rows))
+            count = 0;
+            # self.lineEdit_3.setText("1")
+            # self.lineEdit_4.setText(str(len(rows)))
+            # self.pushButton.setDisabled(1)
+            for row in rows:
+                self.tableWidget.setItem(count, 0, QTableWidgetItem(str(row[0])))
+                self.tableWidget.setItem(count, 1, QTableWidgetItem(row[1]))
+                # self.tableWidget.setItem(count, 2, QTableWidgetItem(row[2]))
+                # self.tableWidget.setItem(count, 3, QTableWidgetItem(row[3]))
+                count += 1
+            self.tableWidget.selectRow(0)
+
+        except Exception as e:
+            print(e)
+            print(type(e))
+            self.statusbar.showMessage("[Error] csv 파일을 읽어오는데 실패했습니다." + str(type(e)))
+            return
+
 
 
 if __name__ == "__main__":
